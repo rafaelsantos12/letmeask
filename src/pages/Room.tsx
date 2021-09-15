6a0bd5f5ca990c96affd0588f45b9cc9ed561625
Rafael <r12rafasantos@gmail.com>
2021-09-15 00:20:58 -0300

    Style sala pergunda

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,19 +1,30 @@
+import { useParams } from 'react-router';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+import '../styles/room.scss'
+
+
+type RoomParams ={
+    id: string;
+}
 
 export  function Room() {
+
+    const params = useParams<RoomParams>();
+
     return(
         <div id="page-room">
-            <head>
+            <header>
                 <div className="content">
-                    <img src={logoImg} alt="Letmeask" />
-                    <div>
-                        código
-                    </div>
+                  <img src={logoImg} alt="letmeask" />
+            
+                        <RoomCode code={params.id}/>
+           
                 </div>
-            </head>
+            </header>
 
-            <main className="content">
+            <main >
                 <div className="room-title">
                     <h1>Sala React</h1>
                     <span>4 perguntas</span>
